5f62d345dd669ab674153163c19d9e9ced1970d1
Stephen <stephen.c@bankbazaar.com>
2017-03-15 18:23:55 +0530

    changes

--- extractLogData.pl
+++ extractLogData.pl
@@ -4,17 +4,18 @@ use strict;
 use JSON;
 use Data::Dumper;
 
-my $gte =  1488738000000;
-my $lte =  1488738010000;
-my $home = '/LogBackups/s4_backup';
+my $home = '/home/csteve/s3_backup';
+my $lte = 1489257000000;
+my $final_lte = 1489343400000;
+my $date = "mar12";
 
-while ($lte <= (1488825800000)){
+my $gte;
+while ($lte < $final_lte){
 $gte = $lte;
 $lte = $gte + 1000;
-
 my $cmd = qq{ 
 
-curl -XGET 'http://10.1.20.44:9200/*bankbridgeservice*/_search?pretty&filter_path=hits.hits._source' -d  '{
+curl -XGET 'http://10.1.20.45:9200/*bankbridgeservice*/_search?pretty&filter_path=hits.hits._source' -d  '{
 "size" : 10000,
 "_source":  {
        "includes": ["\@timestamp", "message", "source"]
@@ -53,14 +54,14 @@ my $data = decode_json ` $cmd `;
 foreach my $hit (@{$data->{'hits'}->{'hits'}}) {
 
 if ($hit->{'_source'}->{'source'} =~ /bankbridge-service.log/i) {
-    open(my $fd, ">> $home/bankbridge-service_mar6.txt") or die $!;
+    open(my $fd, ">> $home/bankbridge-service_$date.txt") or die $!;
     print localtime() . $hit->{'_source'}->{'source'} . "\n" ;
     print $fd $hit->{'_source'}->{'message'} . "\n" ;
     close $fd;
 }
 
 if ($hit->{'_source'}->{'source'} =~ /warehouse/i) {
-    open(my $fd, ">> $home/warehouse_mar6.txt") or die $1;
+    open(my $fd, ">> $home/warehouse_$date.txt") or die $1;
     print localtime() . $hit->{'_source'}->{'source'} . "\n" ;
     print $fd $hit->{'_source'}->{'message'} . "\n" ;
     close $fd;
